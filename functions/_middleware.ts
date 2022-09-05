2f73befee0f8fd6c89cbe4414d8feaa0125f6834
vsamaru <vsamaru@gmail.com>
2022-09-05 23:22:18 +0300

    -

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,9 +1,9 @@
-import "./console.js";
+import "./console.ts";
 
 export const onRequest = async ({ next }) => {
 
 console.info(1)
-	
+
   const response = await next();
   response.headers.set("x-custom", "header value");
   return response;
